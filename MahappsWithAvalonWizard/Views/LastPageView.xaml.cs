dd87597b42c22175a46684ed173357973f7f0945
Jan Karger <punkerat76@gmail.com>
2016-10-05 09:40:39 +0200

    Use Loaded/Unloaded events to work ccorrect with the wizard page   The page Xaml isn't created at the initialize command, so you can't use the DialogCoordinator at this point.   I added a new interface with a LoadedCommand which can you implement at the ViewModel and call at the Xaml code behind.

--- MahappsWithAvalonWizard/Views/LastPageView.xaml.cs
+++ MahappsWithAvalonWizard/Views/LastPageView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace MahappsWithAvalonWizard
 {
@@ -23,6 +24,17 @@ namespace MahappsWithAvalonWizard
         public LastPageView()
         {
             InitializeComponent();
+            this.Loaded += (sender, args) =>
+            {
+                DialogParticipation.SetRegister(this, this.DataContext);
+                ((IWizardPageLoadableViewModel) this.DataContext).LoadedCommand.Execute(this);
+            };
+            // if using DialogParticipation on Windows which open / close frequently you will get a
+            // memory leak unless you unregister.  The easiest way to do this is in your Closing/ Unloaded
+            // event, as so:
+            //
+            // DialogParticipation.SetRegister(this, null);
+            this.Unloaded += (sender, args) => { DialogParticipation.SetRegister(this, null); };
         }
     }
 }
